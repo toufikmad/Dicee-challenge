87d88948576f09360070b4a65108c8f5936b8dc4
toufikmad <114244771+toufikmad@users.noreply.github.com>
2022-09-23 17:20:48 +0200

    Add files via upload

---
+++ Dicee Challenge/javascript.js
@@ -0,0 +1,29 @@
+// frst_img
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //number 1-6
+
+var randomDiceImage = "images/dice" + randomNumber1 + ".png"; // images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src" , randomDiceImage);
+
+
+//scnd_img
+var randomNumber2 = Math.floor(Math.random() * 6) + 1 ;
+
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src" , randomDiceImage2);
+
+
+//the_title
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 wins";
+}else if(randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 wins";
+} else {   
+    document.querySelector("h1").innerHTML = "Equal";
+}
